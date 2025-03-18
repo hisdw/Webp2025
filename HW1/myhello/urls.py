6bd6660c157aa18f4bfc6ecfac9b414a372b6234
eN <127071390+hisdw@users.noreply.github.com>
2025-03-18 21:44:25 +0800

    Delete HW1 directory

--- HW1/myhello/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path 
-from . import views
-
-urlpatterns=[
-    path('addcourse', views.addcourse_post, name='add_post'),
-    path('courselist', views.courselist_post, name='list_post'),
-]
